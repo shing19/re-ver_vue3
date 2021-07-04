5fd0574b43ff80f549cf7b475e9c68de434c99ba
vasopikof <jinklub.krid@gmail.com>
2021-07-04 19:28:39 +0700

    Add lazy loader + staging dockerfile for nginx

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import installElementPlus from './plugins/element'
+import VueLazyLoad from 'vue3-lazyload'
 
 const app = createApp(App)
+app.use(VueLazyLoad, {
+    loading: () => {
+      console.log('loading')
+    },
+    error: () => {
+      console.log('error')
+    },
+    loaded: () => {
+      console.log('loaded')
+    }
+})
 installElementPlus(app)
 app.use(router)
 app.mount('#app')
